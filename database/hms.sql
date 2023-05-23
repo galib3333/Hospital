fd095e219fe7764a3e196d9739fc63c0d7289013
Fuad1610006 <afmfuad96@gmail.com>
2023-05-23 13:34:25 +0600

    department add

--- database/hms.sql
+++ database/hms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2023 at 06:07 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: May 23, 2023 at 09:33 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,50 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Table structure for table `departments`
 --
 
-CREATE TABLE `admin` (
+CREATE TABLE `departments` (
   `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `updationDate` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `department` varchar(255) NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `admin`
+-- Dumping data for table `departments`
 --
 
-INSERT INTO `admin` (`id`, `username`, `password`, `updationDate`) VALUES
-(1, 'admin', 'Test@12345', '30-10-2022 11:42:05 AM');
+INSERT INTO `departments` (`id`, `department`, `deleted_at`) VALUES
+(1, 'Cardiology', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `appointment`
+-- Table structure for table `designations`
 --
 
-CREATE TABLE `appointment` (
+CREATE TABLE `designations` (
   `id` int(11) NOT NULL,
-  `doctorSpecialization` varchar(255) DEFAULT NULL,
-  `doctorId` int(11) DEFAULT NULL,
-  `userId` int(11) DEFAULT NULL,
-  `consultancyFees` int(11) DEFAULT NULL,
-  `appointmentDate` varchar(255) DEFAULT NULL,
-  `appointmentTime` varchar(255) DEFAULT NULL,
-  `postingDate` timestamp NULL DEFAULT current_timestamp(),
-  `userStatus` int(11) DEFAULT NULL,
-  `doctorStatus` int(11) DEFAULT NULL,
-  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `designation` varchar(255) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `appointment`
+-- Dumping data for table `designations`
 --
 
-INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `consultancyFees`, `appointmentDate`, `appointmentTime`, `postingDate`, `userStatus`, `doctorStatus`, `updationDate`) VALUES
-(1, 'ENT', 1, 1, 500, '2022-11-10', '12:45 PM', '2022-11-06 12:21:48', 1, 0, '2022-11-06 12:23:35'),
-(2, 'ENT', 1, 2, 500, '2022-11-17', '7:00 PM', '2022-11-06 13:16:18', 1, 1, NULL);
+INSERT INTO `designations` (`id`, `designation`, `deleted_at`) VALUES
+(1, 'Intern Doctor up', NULL);
 
 -- --------------------------------------------------------
 
@@ -77,216 +67,18 @@ INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `
 
 CREATE TABLE `doctors` (
   `id` int(11) NOT NULL,
-  `specilization` varchar(255) DEFAULT NULL,
-  `doctorName` varchar(255) DEFAULT NULL,
-  `address` longtext DEFAULT NULL,
-  `docFees` varchar(255) DEFAULT NULL,
-  `contactno` bigint(11) DEFAULT NULL,
-  `docEmail` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `creationDate` timestamp NULL DEFAULT current_timestamp(),
-  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `doctors`
---
-
-INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `address`, `docFees`, `contactno`, `docEmail`, `password`, `creationDate`, `updationDate`) VALUES
-(1, 'ENT', 'Anuj kumar', 'A 123 XYZ Apartment Raj Nagar Ext Ghaziabad', '500', 142536250, 'anujk123@test.com', 'f925916e2754e5e03f75dd58a5733251', '2022-10-30 18:16:52', '2022-11-06 13:20:17'),
-(2, 'Endocrinologists', 'Charu Dua', 'X 1212 ABC Apartment Laxmi Nagar New Delhi ', '800', 1231231230, 'charudua12@test.com', 'f925916e2754e5e03f75dd58a5733251', '2022-11-04 01:06:41', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `doctorslog`
---
-
-CREATE TABLE `doctorslog` (
-  `id` int(11) NOT NULL,
-  `uid` int(11) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `userip` binary(16) DEFAULT NULL,
-  `loginTime` timestamp NULL DEFAULT current_timestamp(),
-  `logout` varchar(255) DEFAULT NULL,
-  `status` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `doctorslog`
---
-
-INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
-(20, NULL, 'gfdgdf', 0x3a3a3100000000000000000000000000, '2022-11-04 01:02:16', NULL, 0),
-(21, 2, 'charudua12@test.com', 0x3a3a3100000000000000000000000000, '2022-11-06 11:59:40', '06-11-2022 05:35:18 PM', 1),
-(22, 2, 'charudua12@test.com', 0x3a3a3100000000000000000000000000, '2022-11-06 12:06:37', '06-11-2022 05:36:40 PM', 1),
-(23, 2, 'charudua12@test.com', 0x3a3a3100000000000000000000000000, '2022-11-06 12:08:56', '06-11-2022 05:42:53 PM', 1),
-(24, 1, 'anujk123@test.com', 0x3a3a3100000000000000000000000000, '2022-11-06 12:23:18', '06-11-2022 05:53:40 PM', 1),
-(25, 2, 'charudua12@test.com', 0x3a3a3100000000000000000000000000, '2022-11-06 13:16:53', '06-11-2022 06:47:07 PM', 1),
-(26, 1, 'anujk123@test.com', 0x3a3a3100000000000000000000000000, '2022-11-06 13:17:33', '06-11-2022 06:50:28 PM', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `doctorspecilization`
---
-
-CREATE TABLE `doctorspecilization` (
-  `id` int(11) NOT NULL,
-  `specilization` varchar(255) DEFAULT NULL,
-  `creationDate` timestamp NULL DEFAULT current_timestamp(),
-  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `doctorspecilization`
---
-
-INSERT INTO `doctorspecilization` (`id`, `specilization`, `creationDate`, `updationDate`) VALUES
-(1, 'Orthopedics', '2022-10-30 18:09:46', NULL),
-(2, 'Internal Medicine', '2022-10-30 18:09:57', NULL),
-(3, 'Obstetrics and Gynecology', '2022-10-30 18:10:18', NULL),
-(4, 'Dermatology', '2022-10-30 18:10:28', NULL),
-(5, 'Pediatrics', '2022-10-30 18:10:37', NULL),
-(6, 'Radiology', '2022-10-30 18:10:46', NULL),
-(7, 'General Surgery', '2022-10-30 18:10:56', NULL),
-(8, 'Ophthalmology', '2022-10-30 18:11:03', NULL),
-(9, 'Anesthesia', '2022-10-30 18:11:15', NULL),
-(10, 'Pathology', '2022-10-30 18:11:22', NULL),
-(11, 'ENT', '2022-10-30 18:11:30', NULL),
-(12, 'Dental Care', '2022-10-30 18:11:39', NULL),
-(13, 'Dermatologists', '2022-10-30 18:12:02', NULL),
-(14, 'Endocrinologists', '2022-10-30 18:12:10', NULL),
-(15, 'Neurologists', '2022-10-30 18:12:30', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblcontactus`
---
-
-CREATE TABLE `tblcontactus` (
-  `id` int(11) NOT NULL,
-  `fullname` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `contactno` bigint(12) DEFAULT NULL,
-  `message` mediumtext DEFAULT NULL,
-  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
-  `AdminRemark` mediumtext DEFAULT NULL,
-  `LastupdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
-  `IsRead` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `tblcontactus`
---
-
-INSERT INTO `tblcontactus` (`id`, `fullname`, `email`, `contactno`, `message`, `PostingDate`, `AdminRemark`, `LastupdationDate`, `IsRead`) VALUES
-(1, 'Anuj kumar', 'anujk30@test.com', 1425362514, 'This is for testing purposes.   This is for testing purposes.This is for testing purposes.This is for testing purposes.This is for testing purposes.This is for testing purposes.This is for testing purposes.This is for testing purposes.This is for testing purposes.', '2022-10-30 16:52:03', NULL, NULL, NULL),
-(2, 'Anuj kumar', 'ak@gmail.com', 1111122233, 'This is for testing', '2022-11-06 13:13:41', 'Contact the patient', '2022-11-06 13:13:57', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblmedicalhistory`
---
-
-CREATE TABLE `tblmedicalhistory` (
-  `ID` int(10) NOT NULL,
-  `PatientID` int(10) DEFAULT NULL,
-  `BloodPressure` varchar(200) DEFAULT NULL,
-  `BloodSugar` varchar(200) NOT NULL,
-  `Weight` varchar(100) DEFAULT NULL,
-  `Temperature` varchar(200) DEFAULT NULL,
-  `MedicalPres` mediumtext DEFAULT NULL,
-  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `tblmedicalhistory`
---
-
-INSERT INTO `tblmedicalhistory` (`ID`, `PatientID`, `BloodPressure`, `BloodSugar`, `Weight`, `Temperature`, `MedicalPres`, `CreationDate`) VALUES
-(1, 1, '80/120', '120', '85', '98', 'Test', '2022-11-06 13:19:41');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblpage`
---
-
-CREATE TABLE `tblpage` (
-  `ID` int(10) NOT NULL,
-  `PageType` varchar(200) DEFAULT NULL,
-  `PageTitle` varchar(200) DEFAULT NULL,
-  `PageDescription` mediumtext DEFAULT NULL,
-  `Email` varchar(120) DEFAULT NULL,
-  `MobileNumber` bigint(10) DEFAULT NULL,
-  `UpdationDate` timestamp NULL DEFAULT current_timestamp(),
-  `OpenningTime` varchar(255) DEFAULT NULL
+  `name` varchar(255) NOT NULL,
+  `designation_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `specialist` varchar(255) DEFAULT NULL,
+  `education` varchar(255) DEFAULT NULL,
+  `fees` decimal(10,2) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tblpage`
---
-
-INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`, `OpenningTime`) VALUES
-(1, 'aboutus', 'About Us', '<ul style=\"padding: 0px; margin-right: 0px; margin-bottom: 1.313em; margin-left: 1.655em;\" times=\"\" new=\"\" roman\";=\"\" font-size:=\"\" 14px;=\"\" text-align:=\"\" center;=\"\" background-color:=\"\" rgb(255,=\"\" 246,=\"\" 246);\"=\"\"><li style=\"text-align: left;\"><font color=\"#000000\">The Hospital Management System (HMS) is designed for Any Hospital to replace their existing manual, paper based system. The new system is to control the following information; patient information, room availability, staff and operating room schedules, and patient invoices. These services are to be provided in an efficient, cost effective manner, with the goal of reducing the time and resources currently required for such tasks.</font></li><li style=\"text-align: left;\"><font color=\"#000000\">A significant part of the operation of any hospital involves the acquisition, management and timely retrieval of great volumes of information. This information typically involves; patient personal information and medical history, staff information, room and ward scheduling, staff scheduling, operating theater scheduling and various facilities waiting lists. All of this information must be managed in an efficient and cost wise fashion so that an institution\'s resources may be effectively utilized HMS will automate the management of the hospital making it more efficient and error free. It aims at standardizing data, consolidating data ensuring data integrity and reducing inconsistencies.&nbsp;</font></li></ul>', NULL, NULL, '2020-05-20 07:21:52', NULL),
-(2, 'contactus', 'Contact Details', 'D-204, Hole Town South West, Delhi-110096,India', 'info@gmail.com', 1122334455, '2020-05-20 07:24:07', '9 am To 8 Pm');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblpatient`
---
-
-CREATE TABLE `tblpatient` (
-  `ID` int(10) NOT NULL,
-  `Docid` int(10) DEFAULT NULL,
-  `PatientName` varchar(200) DEFAULT NULL,
-  `PatientContno` bigint(10) DEFAULT NULL,
-  `PatientEmail` varchar(200) DEFAULT NULL,
-  `PatientGender` varchar(50) DEFAULT NULL,
-  `PatientAdd` mediumtext DEFAULT NULL,
-  `PatientAge` int(10) DEFAULT NULL,
-  `PatientMedhis` mediumtext DEFAULT NULL,
-  `CreationDate` timestamp NULL DEFAULT current_timestamp(),
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `tblpatient`
---
-
-INSERT INTO `tblpatient` (`ID`, `Docid`, `PatientName`, `PatientContno`, `PatientEmail`, `PatientGender`, `PatientAdd`, `PatientAge`, `PatientMedhis`, `CreationDate`, `UpdationDate`) VALUES
-(1, 1, 'Amit Kumar', 1231231230, 'amitk@gmail.com', 'male', 'New Delhi india', 35, 'NA', '2022-11-06 13:18:31', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userlog`
---
-
-CREATE TABLE `userlog` (
-  `id` int(11) NOT NULL,
-  `uid` int(11) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `userip` binary(16) DEFAULT NULL,
-  `loginTime` timestamp NULL DEFAULT current_timestamp(),
-  `logout` varchar(255) DEFAULT NULL,
-  `status` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `userlog`
---
-
-INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
-(1, 1, 'johndoe12@test.com', 0x3a3a3100000000000000000000000000, '2022-11-06 12:14:11', NULL, 1),
-(2, 1, 'johndoe12@test.com', 0x3a3a3100000000000000000000000000, '2022-11-06 12:21:20', '06-11-2022 05:53:00 PM', 1),
-(3, NULL, 'amitk@gmail.com', 0x3a3a3100000000000000000000000000, '2022-11-06 13:15:43', NULL, 0),
-(4, 2, 'amitk@gmail.com', 0x3a3a3100000000000000000000000000, '2022-11-06 13:15:58', '06-11-2022 06:50:46 PM', 1),
-(5, 1, 'johndoe12@test.com', 0x3a3a3100000000000000000000000000, '2023-05-21 15:09:24', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -296,38 +88,39 @@ INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`,
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `fullName` varchar(255) DEFAULT NULL,
-  `address` longtext DEFAULT NULL,
-  `city` varchar(255) DEFAULT NULL,
-  `gender` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `regDate` timestamp NULL DEFAULT current_timestamp(),
-  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contact_no` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`, `regDate`, `updationDate`) VALUES
-(1, 'John Doe', 'A 123 ABC Apartment GZB 201017', 'Ghaziabad', 'male', 'johndoe12@test.com', 'f925916e2754e5e03f75dd58a5733251', '2022-11-06 12:13:56', NULL),
-(2, 'Amit kumar', 'new Delhi india', 'New Delhi', 'male', 'amitk@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2022-11-06 13:15:32', NULL);
+INSERT INTO `users` (`id`, `name`, `email`, `contact_no`, `password`, `image`, `created_by`, `created_at`, `updated_by`, `updated_at`, `deleted_at`) VALUES
+(1, 'Kamal Uddin', 'kamal@yahoo.com', '', 'adcd7048512e64b48da55b027577886ee5a36350', NULL, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin`
+-- Indexes for table `departments`
 --
-ALTER TABLE `admin`
+ALTER TABLE `departments`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `appointment`
+-- Indexes for table `designations`
 --
-ALTER TABLE `appointment`
+ALTER TABLE `designations`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -337,123 +130,39 @@ ALTER TABLE `doctors`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `doctorslog`
---
-ALTER TABLE `doctorslog`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `doctorspecilization`
---
-ALTER TABLE `doctorspecilization`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tblcontactus`
---
-ALTER TABLE `tblcontactus`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tblmedicalhistory`
---
-ALTER TABLE `tblmedicalhistory`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `tblpage`
---
-ALTER TABLE `tblpage`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `tblpatient`
---
-ALTER TABLE `tblpatient`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `userlog`
---
-ALTER TABLE `userlog`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT for table `departments`
 --
-ALTER TABLE `admin`
+ALTER TABLE `departments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `appointment`
+-- AUTO_INCREMENT for table `designations`
 --
-ALTER TABLE `appointment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `designations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `doctors`
 --
 ALTER TABLE `doctors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `doctorslog`
---
-ALTER TABLE `doctorslog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
-
---
--- AUTO_INCREMENT for table `doctorspecilization`
---
-ALTER TABLE `doctorspecilization`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- AUTO_INCREMENT for table `tblcontactus`
---
-ALTER TABLE `tblcontactus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `tblmedicalhistory`
---
-ALTER TABLE `tblmedicalhistory`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `tblpage`
---
-ALTER TABLE `tblpage`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `tblpatient`
---
-ALTER TABLE `tblpatient`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `userlog`
---
-ALTER TABLE `userlog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ department_edit.php
@@ -0,0 +1,78 @@
+<?php include_once('include/header.php'); ?>
+<?php include_once('include/sidebar.php'); ?>
+<!-- ============================================================== -->
+<!-- Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="page-breadcrumb">
+    <div class="row">
+        <div class="col-12 d-flex no-block align-items-center">
+            <h4 class="page-title">Department</h4>
+            <div class="ms-auto text-end">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#<?= $base_url?>dashboard.php">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Department Update</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- Container fluid  -->
+<!-- ============================================================== -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <form class="form-horizontal" action="" method="post">
+                <?php
+                  $where['id']=$_GET['id'];
+                  $data=$mysqli->common_select('departments','*',$where);
+                 
+                  if(!$data['error'] && count($data['data'])>0)
+                    $d=$data['data'][0];
+                  else{
+                    echo "<h2 class='text-danger text-center'>This url is not correct</h2>";
+                    
+                  }
+                ?>
+                    <div class="card-body">
+                        <h4 class="card-title">Department Information</h4>
+                        <div class="form-group row">
+                            <label for="department" class="col-sm-3 text-end control-label col-form-label">Department</label>
+                            <div class="col-sm-9">
+                                <input type="text" value="<?= $d->department ?>" class="form-control" id="department" name="department">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="border-top">
+                        <div class="card-body">
+                            <button type="button" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                </form>
+                <?php
+                    if($_POST){
+                        $rs=$mysqli->common_update('departments',$_POST,$where);
+                        print_r($rs);
+                        if(!$rs['error']){
+                        echo "<script>window.location='department_list.php'</script>";
+                        }else{
+                            echo $rs['error'];
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- ============================================================== -->
+<!-- End Container fluid  -->
+<!-- ============================================================== -->
+            
+<?php include_once('include/footer.php'); ?>
