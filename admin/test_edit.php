6413ef40abd447b66c72f98d72cb0c17a662a70c
galib3333 <ikssakil2@gmail.com>
2023-06-04 13:19:22 +0600

    test updated

--- admin/test_edit.php
+++ admin/test_edit.php
@@ -6,12 +6,12 @@
 <div class="page-breadcrumb">
     <div class="row">
         <div class="col-12 d-flex no-block align-items-center">
-            <h4 class="page-title">Doctors</h4>
+            <h4 class="page-title">Tests</h4>
             <div class="ms-auto text-end">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="#<?= $base_url?>dashboard.php">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Doc's info Update</li>
+                        <li class="breadcrumb-item active" aria-current="page">Test info Update</li>
                     </ol>
                 </nav>
             </div>
