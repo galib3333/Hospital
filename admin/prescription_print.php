04bf3975672c535de06ed6fe440c020d5a13e8ba
kaiser-hoque2 <isbdstudent@gmail.com>
2023-06-12 11:51:43 +0600

    edited

--- admin/prescription_print.php
+++ admin/prescription_print.php
@@ -9,53 +9,75 @@
     $mysqli=new crud;
 ?>
 
-  <style>
-    body{
-      font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-      
+<style>
+  body {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    margin: 0;
+  }
+
+  h1 {
+    color: rgb(77,209,253);
+  }
+
+  h3 {
+    color: rgb(77,209,253);
+  }
+
+  .border {
+    border-bottom: 5px solid rgb(77,209,253);
+    padding: 10px;
+  }
+
+  .h1 {
+    opacity: 0.2;
+    font-size: 80px;
+    transform: rotate(-30deg);
+  }
+
+  strong {
+    padding: 2%!important;
+  }
+
+  p {
+    padding: 1%;
+    text-align: center;
+    background-color: rgb(77,209,253);
+  }
+
+  .sign {
+    height: 300px;
+  }
+
+  /* footer {
+    width: 100%;
+    background-color: aqua;
+    
+    bottom: 0;
+    left: 0;
+    height: 40px;
+  } */
+  .print-button {
+    padding: 10px 20px;
+    font-size: 14px;
+    background-color: rgb(77,209,253);
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  @media print {
+    button {
+      display: none;
     }
-   h1{
-    color: aqua;
-   }
-    h3{
-      color:aqua;
-    }
-    .border{
-      border-bottom: 5px solid aqua;
-      padding: 10px;
-     }
-     .h1{
-      opacity:0.2;
-     font-size: 80px;
-     transform: rotate(-30deg);
-     }
-     strong{
-      padding:2%!important;
-     }
-     p{
-      padding:1%;
-      text-align: center;
-      background-color: aqua;
-     }
-     .prescription{
-      height: 300px;
-     }
-     footer{
-      width: 100%;
-      background-color: aqua;
-      position: fixed;
-      bottom: 0;
-      left: 0;
-      
-     }
-    @media print {
-      button{
-        display: none;
-      }
-    }
-     
-  </style>
-<button type="button" onclick="printbtn()">Print</button>
+  }
+</style>
+
+<button type="button" class="print-button" onclick="printbtn()">Print</button>
 <?php
   $data=$mysqli->common_select_query("SELECT patients.name, patients.sex, doctors.name as doc,doctors.email,doctors.specialist,doctors.education, p_prescription.* FROM `p_prescription` 
   join patients on patients.id=p_prescription.patient_id
@@ -76,7 +98,7 @@
 ?>
   <section style="display: flex; justify-content: space-between; align-items: center; padding: 20px;">
     <div style="text-align: left;">
-        <h1>XYZ Hospital</h1>
+        <h1>Matrix Hospital</h1>
     </div>
     <div></div>
     <div style="text-align: right;">
@@ -92,7 +114,7 @@
      <strong>Gender :<span style="border-bottom:1px dotted"><?= $prescription->sex ?></span></strong>
      <strong>Date :<span style="border-bottom:1px dotted"><?= date('d/m/Y',strtotime($prescription->created_at)) ?></span></strong>
    </div>
-    <table style="width:100%">
+    <table style="width:100%; padding-left:1%">
       <tr>
         <td style="width:20%">
           <b>Inv.</b>
@@ -123,18 +145,19 @@
         </td>
       </tr>
     </table>
-      
-      
     </div>
-    <div class="prescription">
-     
+    <div class="sign" style="text-align: right; margin-top: 50px;padding-right:1%;">
+      <div style="border-bottom: 2px dotted black; width: 150px; height: 50px; margin-left: auto;"></div>
+     <div>
+     <h4 style="padding-right:3%;">Signature</h4>
+     </div>
     </div>
-    <footer>
-      <p>2no Gate Sholoshahar,Chittagong</p>
-    </footer>
-
+    <!-- <footer>
+      <p style="margin-bottom:10px;">2no Gate Sholoshahar,Chittagong</p>
+    </footer> -->
 <script>
   function printbtn(){
     window.print();
   }
 </script>
+<?php require_once('include/footer.php'); ?>
