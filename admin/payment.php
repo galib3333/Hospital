100d9609fc2f45cbf97d12f9fa34c2101005f7bf
galib3333 <asadullahgalib053@gmail.com>
2023-06-18 12:52:34 -0600

    payment

---
+++ admin/payment.php
@@ -0,0 +1,39 @@
+<?php include_once('include/header.php'); ?>
+<?php include_once('include/sidebar.php'); ?>
+<!-- ============================================================== -->
+<!-- Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="page-breadcrumb">
+    <div class="row">
+        <div class="col-12 d-flex no-block align-items-center">
+            <h4 class="page-title">Payment</h4>
+            <div class="ms-auto text-end">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="<?= $base_url?>dashboard.php">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Payment</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- Container fluid  -->
+<!-- ============================================================== -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Total Amount : BDT  </h1>
+            <h1>Due Amount : </h1>
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Container fluid  -->
+<!-- ============================================================== -->
+            
+<?php include_once('include/footer.php'); ?>
